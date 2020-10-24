f0d990b3b8b1d01c4484d6d714052570cf3c8f10
fnfarias97 <fnfarias97@gmail.com>
2020-10-23 21:10:10 -0300

    fixed styles

--- click-players/controllers/authController.js
+++ click-players/controllers/authController.js
@@ -2,7 +2,7 @@ let userController = {
     
     ingresar : (req, res) => res.render('users/ingresar', { title: 'Click Players | Ingresa a tu cuenta' }),
 
-    registrar : (req, res) => res.render('users/registrar', { title: 'Click Players | Registrate' })
+    registrar : (req, res) => res.render('users/registrar', { title: 'Click Players | Registrate', stylesheet: 'registrar' })
 };
 
 module.exports = userController;
--- click-players/controllers/mainController.js
+++ click-players/controllers/mainController.js
@@ -1,8 +1,8 @@
 let mainController = {
     
-    home : (req, res) => res.render('index', { title: 'Click Players | Home' }),
+    home : (req, res) => res.render('index', { title: 'Click Players | Home', stylesheet: 'index' }),
 
-    contacto : (req, res) => res.render('contacto', { title: 'Click Players | Contacto' })
+    contacto : (req, res) => res.render('contacto', { title: 'Click Players | Contacto', stylesheet: 'contacto' })
 };
 
 module.exports = mainController;
