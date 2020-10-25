22fd8b404639f661f430464a09b54d697e7e073c
Nahuel-Ben <nahuel.b13@gmail.com>
2020-10-25 14:19:44 -0300

    completados algunos ejs y algunos css y sus rutas correspondientes

--- click-players/controllers/authController.js
+++ click-players/controllers/authController.js
@@ -1,6 +1,6 @@
 let userController = {
     
-    ingresar : (req, res) => res.render('users/ingresar', { title: 'Click Players | Ingresa a tu cuenta' }),
+    ingresar : (req, res) => res.render('users/ingresar', { title: 'Click Players | Ingresa a tu cuenta', stylesheet: 'ingresar' }),
 
     registrar : (req, res) => res.render('users/registrar', { title: 'Click Players | Registrate', stylesheet: 'registrar' })
 };
--- click-players/controllers/productsController.js
+++ click-players/controllers/productsController.js
@@ -1,10 +1,10 @@
 let productsController = {
     
-    products : (req, res) => res.render('products/productos', { title: 'Click Players | Productos' }),
+    products : (req, res) => res.render('products/productos', { title: 'Click Players | Productos', stylesheet: 'index' }),
 
-    detalle : (req, res) => res.render('products/detalle', { title: 'Click Players | Detalle del producto' }),
+    detalle : (req, res) => res.render('products/detalle', { title: 'Click Players | Detalle del producto', stylesheet: 'detalle' }),
 
-    carrito : (req, res) => res.render('products/carrito', { title: 'Click Players | Carrito de productos'})
+    carrito : (req, res) => res.render('products/carrito', { title: 'Click Players | Carrito de productos', stylesheet: 'carrito'})
 };
 
 module.exports = productsController;
