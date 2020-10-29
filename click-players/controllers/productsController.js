ed561950a51764329978c4876ebb38dba33fee68
fnfarias97 <fnfarias97@gmail.com>
2020-10-29 00:53:25 -0300

    updated carrito

--- click-players/controllers/productsController.js
+++ click-players/controllers/productsController.js
@@ -5,34 +5,51 @@ let productsController = {
     products : (req, res) => res.render('products/productos', { title: 'Click Players | Productos', stylesheet: 'index' }),
     
     show: (req, res) => {
-        let a = products.find((product) => {
-            return product.id == req.params.id
-        })
+        let producto = products.find((e) => e.id == req.params.id);
+        producto.precio = producto.precio.toString() . replace( /\B(?=(\d{3})+(?!\d))/g,
+        "." ) ;
 
-        if (a){
-            res.render('products/detalle', { title: 'Click Players | Detalle del producto', stylesheet: 'detalle', productNombre : a.nombre, productPrecio : a.precio, productDescription: a.descripcion, productImage : a.imagen})
+        if (producto != undefined){
+            res.render('products/detalle', { title: 'Click Players | Detalle del producto', stylesheet: 'detalle', producto})
         }
 
-        res.render('products/detalle', { title: 'Click Players | Detalle del producto', stylesheet: 'detalle', productNombre : 'Lo sentimos',  productPrecio : 'el producto', productDescription: 'no existe', productImage : '/images/producto-no-encontrado.png'})
+        let error = {
+            nombre: 'Lo sentimos',
+            precio: '0',
+            descripcion: 'El producto no existe',
+            avatar: 'producto-no-encontrado.png'
+        }
+
+        res.status(404).render('products/detalle', { title: 'Click Players | Detalle del producto', stylesheet: 'detalle', producto : error})
     },
 
-    agregarProducto: (req, res) => res.render('products/agregarProducto', { title: 'Click Players | Agregar producto', stylesheet: 'ingresar' }),
+    addProduct: (req, res) => res.render('products/agregarProducto', { title: 'Click Players | Agregar producto', stylesheet: 'forms' }),
 
-    editarProducto: (req, res) => {
-        let a = products.find((product) => {
-            return product.id == req.params.id
-        })
+    editProduct: (req, res) => {
+        let producto = products.find((e) => e.id == req.params.id);
+        producto.precio = producto.precio.toString() . replace( /\B(?=(\d{3})+(?!\d))/g,
+        "." ) ;
 
-        if (a){
-            res.render('products/editarProducto', { title: 'Click Players | Modificar producto', stylesheet: 'ingresar', productNombre : a.nombre, productPrecio : a.precio, productDescription: a.descripcion, productImage : a.imagen, productMarca : a.marca, productCategoria : a.categoria})
+        if (producto != undefined){
+            res.render('products/editarProducto', { title: 'Click Players | Modificar producto', stylesheet: 'forms', producto})
         }
 
-        res.render('products/detalle', { title: 'Click Players | Detalle del producto', stylesheet: 'detalle', productNombre : 'Lo sentimos',  productPrecio : 'el producto', productDescription: 'no existe', productImage : '/images/producto-no-encontrado.png'})
+        let error = {
+            nombre: 'Lo sentimos',
+            precio: '0',
+            descripcion: 'El producto no existe',
+            avatar: 'producto-no-encontrado.png'
+        }
+
+        res.status(404).render('products/detalle', { title: 'Click Players | Detalle del producto', stylesheet: 'detalle', producto : error})
     },
 
     detalle : (req, res) => res.render('products/detalle', { title: 'Click Players | Detalle del producto', stylesheet: 'detalle' }),
 
-    carrito : (req, res) => res.render('products/carrito', { title: 'Click Players | Carrito de productos', stylesheet: 'carrito'})
-};
+    carrito : (req, res) => {
+        let productsCart = [...products]
+        res.render('products/carrito', { title: 'Click Players | Carrito de productos', stylesheet: 'carrito', products : productsCart})
+    }
+}
 
 module.exports = productsController;
